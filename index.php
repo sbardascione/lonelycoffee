804c1a635b19c53406f756ee528216f31ea0784c
sbardascione <crmsal81@gmail.com>
2018-03-24 11:54:03 +0100

    changing pointing to favicon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 
 <head>
-   <link rel="shortcut icon" href="http://faviconist.com/icons/4d2ce5e76747cdcf1429c8ea135970d7/favicon.ico" />
+   <link rel="shortcut icon" href="./images/favicon.ico" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
