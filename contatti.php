6ee8c01fddf8142fed15e569ac52429312e8f2af
sbardascione <crmsal81@gmail.com>
2018-03-24 11:49:03 +0100

    changing contacts info

--- contatti.php
+++ contatti.php
@@ -3,7 +3,7 @@
 			<i class="fa fa-phone fa-2x"></i>&nbsp;&nbsp;+39 340 0945132&nbsp;/&nbsp;+39 328 5583238
 		</span>
 		<span class="list-group-item">
-			<i class="fa fa-envelope fa-2x"></i>&nbsp;&nbsp;Teleborderline Produzioni: <a href="mailto:<teleborderline@gmail.com>">teleborderline@gmail.com</a>
+			<i class="fa fa-envelope fa-2x"></i>&nbsp;&nbsp;Info: <a href="mailto:<info@lonelycoffee.it>">info@lonelycoffee.it</a>
 		</span>
 
 	</div>	
